37160fbc8aef340854e0f3e721248821684e1605
Mahdi Dibaiee <mdibaiee@aol.com>
2015-10-15 11:57:17 +0330

    chore(tests, config): configure mocha, istanbul, codecov, travis, ghooks, commitizen and write initial tests and methods
    Implement #match, #sendMessage and #icon on Bot

--- src/bot.js
+++ src/bot.js
@@ -2,15 +2,65 @@ import config from './config';
 import SlackBot from 'slackbots';
 
 export default class Bot extends SlackBot {
+  /**
+   * Takes slackbots config object containing token and name
+   *
+   * Emits different events for each event with 'got:' prefix, e.g. 'got:typing'
+   * @param  {object} config slackbots configs, {token, name}
+   */
   constructor(config) {
     super(config);
+
+    this.on('start', () => {
+      this.on('message', message => {
+        this.emit(`got:${message.type}`, message);
+      });
+    })
+
+    this.globals = {};
   }
 
-  start(fn) {
-    this.on('start', fn);
+  /**
+   * Matches incoming messages against a regular expression using `RegExp.test`
+   * @param  {RegExp}   regex regular expression to match messages against
+   * @param  {Function} fn    the listener, invoked upon a matching message
+   */
+  match(regex, fn) {
+    this.on('got:message', message => {
+      if (regex.test(message.text)) fn(message);
+    })
   }
 
-  sendMessage(text) {
-    this.postMessageToChannel('test-bot', 'Hey');
-  }.filter('send_message')
+  /**
+   * Send a message to IM | Channel | Group
+   * @param  {string} target  The target to send the message to
+   * @param  {string} message Message's text content
+   * @param  {object} params  Message's parameters
+   *                          see https://api.slack.com/methods/chat.postMessage
+   * @return {Promise}        A promise which resolves upon succes and fails
+   *                            in case of errors
+   */
+  sendMessage(target, message, params) {
+    let options = Object.assign({}, this.globals, params);
+    return this.postTo(target, message, options);
+  }
+
+  /**
+   * Set bot's icon, can be either an :emoji: or a url
+   * @param  {string} icon The icon to use, must be formatted like :emoji: to be
+   *                       set as an emoji, otherwise will be considered as URL.
+   *                       Pass a falsy value to delete the property
+   */
+  icon(icon) {
+    if (!icon) {
+      delete this.globals.icon_emoji;
+      delete this.globals.icon_url;
+    }
+
+    if (/:\w+:/.test(icon)) {
+      this.globals.icon_emoji = icon;
+    } else {
+      this.globals.icon_url = icon;
+    }
+  }
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  "token": process.env.BOLT_SLACK_TOKEN,
+  token: process.env.BOLT_SLACK_TOKEN,
   name: 'Bolt'
 }
