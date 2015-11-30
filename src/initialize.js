178e6c8acd6a790a6a128fc424efae493871b463
Mahdi Dibaiee <mdibaiee@aol.com>
2015-11-30 14:44:05 +0330

    chore: setup heroku branch

---
+++ src/initialize.js
@@ -0,0 +1,23 @@
+export default bot => {
+  bot.data = {
+    permissions: {
+      admin: ['mahdi', 'milani'],
+      server: ['milani', 'fattah']
+    },
+    newrelic: {
+      target: 'server-alert',
+      key: process.env.NEWRELIC_KEY,
+      threshold: {
+        error: '>= 2',
+        apdex: '<= 0.7'
+      }
+    },
+    quote: {
+      target: 'test-bolt',
+      every: '30 9 * * *'
+    },
+    leecher: {
+      server: 'http://178.252.151.74:8081/',
+    }
+  };
+}
