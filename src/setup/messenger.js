45015c0889d00692ba204f2a5cb48a11dce3809c
Mahdi Dibaiee <mdibaiee@aol.com>
2016-05-04 11:14:15 +0430

    fix(messenger): messenger was broken because the ports were not used correctly

--- src/setup/messenger.js
+++ src/setup/messenger.js
@@ -3,8 +3,8 @@ import dashboard from './dashboard/index';
 import { get } from 'lodash';
 
 export default bot => {
-  const client = messenger.createSpeaker(get(bot.config, 'dashboard.cport') || 8081);
-  const server = messenger.createListener(get(bot.config, 'dashboard.sport') || 8082);
+  const client = messenger.createSpeaker(get(bot.config, 'dashboard.sport') || 8082);
+  const server = messenger.createListener(get(bot.config, 'dashboard.cport') || 8081);
   bot._dashboard = { client, server };
 
   dashboard(bot, { client, server });
