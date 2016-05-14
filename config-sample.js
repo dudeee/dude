efc88dee71c338400de8ae7a93b29ce2b79c7493
Mahdi Dibaiee <mdibaiee@aol.com>
2016-05-14 15:34:02 +0430

    chore: env variables => DUDE

--- config-sample.js
+++ config-sample.js
@@ -1,7 +1,7 @@
 module.exports = {
-  token: process.env.dude_SLACK_TOKEN,
+  token: process.env.DUDE_SLACK_TOKEN,
   log: {
-    level: process.env.dude_LOG_LEVEL,
+    level: process.env.DUDE_LOG_LEVEL,
     console: true,
     file: false
   }
